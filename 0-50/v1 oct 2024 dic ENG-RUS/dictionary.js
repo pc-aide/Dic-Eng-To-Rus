e35d2a46f5f852d8efdfc6f515aef20472c10b97
pc-aide <36453201+pc-aide@users.noreply.github.com>
2025-10-01 22:48:30 -0400

    Add files via upload

---
+++ 0-50/v1 oct 2024 dic ENG-RUS/dictionary.js
@@ -0,0 +1,19 @@
+// English-Russian dictionary with word numbers
+const dictionary = {
+    1: {
+        english: "family",
+        russian: "семья",
+        image: "img/family.jpg",
+        mp3: "mp3/family.mp3"
+    }
+    // Add more words here
+    // 2: {
+    //     english: "sister",
+    //     russian: "сестра",
+    //     image: "img/sister.jpg",
+    //     mp3: "mp3/sister.mp3"
+    // }
+};
+
+// Number of words in dictionary
+const wordCount = Object.keys(dictionary).length;
